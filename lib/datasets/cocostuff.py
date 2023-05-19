460e646eac7103f81d8271e5759378d0203f722a
elvinado <5048487+elvinado@users.noreply.github.com>
2023-05-19 11:38:33 +0800

    Add required functions

--- lib/datasets/cocostuff.py
+++ lib/datasets/cocostuff.py
@@ -59,6 +59,19 @@ class COCOStuff(BaseDataset):
                     145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 
                     161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 
                     177, 178, 179, 180, 181, 182]
+        
+        self.label_mapping = {-1: ignore_label, 0: ignore_label, 
+                        1: ignore_label, 2: ignore_label, 
+                        3: ignore_label, 4: ignore_label, 
+                        5: ignore_label, 6: ignore_label, 
+                        7: 0, 8: 1, 9: ignore_label, 
+                        10: ignore_label, 11: 2, 12: 3, 
+                        13: 4, 14: ignore_label, 15: ignore_label, 
+                        16: ignore_label, 17: 5, 18: ignore_label, 
+                        19: 6, 20: 7, 21: 8, 22: 9, 23: 10, 24: 11,
+                        25: 12, 26: 13, 27: 14, 28: 15, 
+                        29: ignore_label, 30: ignore_label, 
+                        31: 16, 32: 17, 33: 18}
 
     def read_files(self):
         files = []
@@ -130,3 +143,38 @@ class COCOStuff(BaseDataset):
         image, label = self.gen_sample(image, label, self.multi_scale, self.flip)
 
         return image.copy(), label.copy(), np.array(size), name
+     
+    def convert_label(self, label, inverse=False):
+        temp = label.copy()
+        if inverse:
+            for v, k in self.label_mapping.items():
+                label[temp == k] = v
+        else:
+            for k, v in self.label_mapping.items():
+                label[temp == k] = v
+        return label
+    
+    def get_palette(self, n):
+        palette = [0] * (n * 3)
+        for j in range(0, n):
+            lab = j
+            palette[j * 3 + 0] = 0
+            palette[j * 3 + 1] = 0
+            palette[j * 3 + 2] = 0
+            i = 0
+            while lab:
+                palette[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
+                palette[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
+                palette[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
+                i += 1
+                lab >>= 3
+        return palette
+        
+    def save_pred(self, preds, sv_path, name):
+        palette = self.get_palette(256)
+        preds = np.asarray(np.argmax(preds.cpu(), axis=1), dtype=np.uint8)
+        for i in range(preds.shape[0]):
+            pred = self.convert_label(preds[i], inverse=True)
+            save_img = Image.fromarray(pred)
+            save_img.putpalette(palette)
+            save_img.save(os.path.join(sv_path, name[i]+'.png'))
